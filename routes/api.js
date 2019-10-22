8d7906a22a6bdbac9efd3ee59a23a3d7068750df
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-10-21 20:58:24 -0700

    tests

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
-const { User, Movie } = require('../models');
+const { User } = require('../models');
 const { Types } = require('mongoose');
 
 router.get('/users/:id?', (req, res) => {
     console.log(req.path);
     const id = req.params.id;
-    const promise = id ? User.findOne({_id : Types.ObjectId(id)}) : User.find();
+    const promise = id ? User.findOne({ _id: Types.ObjectId(id) }) : User.find();
     promise.then(results => res.send(results)).catch(err => res.send(err));
+});
+
+router.delete('/user/:id', (req, res) => {
+    const id = req.params.id;
+    User.remove({ _id: Types.ObjectId(id) });
 });
 
 router.post('users', (req, res) => {
--- test/api.js
+++ test/api.js
@@ -1,11 +1,18 @@
 const assert = require('assert');
 const axios = require('axios');
-const { User, Movie } = require('../models');
+const { User } = require('../models');
 
-describe('api', () => {
+describe('GET users', () => {
     it('should return all users', () => {
         Promise.all([axios.get('/api/users'), User.find()]).then((results) => {
             for (let i = 0; i < results[0].length; ++i) assert(results[0][i] == results[1][i]);
         }).catch(err => console.log(err));
     });
-})
+});
+describe('POST users', () => {
+    it('should insert a new user', () => {
+        axios.post('/api/users', { email: 'Bob@bob.bob' }).then(() => {
+            
+        }).catch(err => console.log(err));
+    })
+});
