15a4a3e0e8bc7fd1df441d550266bd73deb22099
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-10-23 19:13:12 -0700

    fixed api routes

--- routes/api.js
+++ routes/api.js
@@ -3,46 +3,46 @@ const { User, Movie } = require('../models');
 const { Types } = require('mongoose');
 
 // Get all users or a specific user by id.
-router.get('users', (req, res) => {
+router.get('/users', (req, res) => {
     console.log(req.path);
-    const id = req.body.id;
+    const id = req.body ? req.body.id : null;
     const promise = id ? User.findById(id) : User.find();
     promise.then(results => res.send(results)).catch(err => res.send(err));
 });
 
 // Delete a user by id.
-router.delete('users', (req, res) => {
-    const id = req.body.id;
+router.delete('/users', (req, res) => {
+    const id = req.body ? req.body.id : null;
     User.findByIdAndDelete(id).then(result => res.send(result)).catch(err => res.send(err));
 });
 
 // Create a new user.
-router.post('users', (req, res) => {
+router.post('/users', (req, res) => {
     User.create(req.body).then(result => res.send(result)).catch(err => res.send(err));
 });
 
 // Get all movies or a specific movie by id.
-router.get('movies', (req, res) => {
+router.get('/movies', (req, res) => {
     console.log(req.path);
-    const id = req.body.id;
+    const id = req.body ? req.body.id : null;
     const promise = id ? Movie.findById(id) : Movie.find();
     promise.then(results => res.send(results)).catch(err => res.send(err));
 });
 
 // Delete a movie by id.
-router.delete('movies', (req, res) => {
-    const id = req.body.id;
+router.delete('/movies', (req, res) => {
+    const id = req.body ? req.body.id : null;
     movie.findByIdAndDelete(id).then(result => res.send(result)).catch(err => res.send(err));
 });
 
 // Create a new movie.
-router.post('movies', (req, res) => {
+router.post('/movies', (req, res) => {
     movie.create(req.body).then(result => res.send(result)).catch(err => res.send(err));
 });
 
 // Add a rating to a user.
-router.put('rating', (req, res) => {
-    const id = req.body.id;
+router.put('/rating', (req, res) => {
+    const id = req.body ? req.body.id : null;
     User.updateOne({ _id: Types.ObjectId(id) }).then(user => {
         user.set(req.body.movie, req.body.rating);
         res.sendStatus(200);
