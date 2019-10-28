a5d1c485b8154eb08e800092e60817e486b4ad43
devcwilkey <DevCWilkey@gmail.com>
2019-10-27 18:07:20 -0700

    Fixing Up Google Auth Redirects

--- routes/index.js
+++ routes/index.js
@@ -4,16 +4,6 @@ const apiRoutes = require("./api");
 const testRoutes = require("./test");
 const authRoutes = require("./auth");
 
-
-// Middleware to check if the user is authenticated
-function isUserAuthenticated(req, res, next) {
-  if (req.user) {
-      next();
-  } else {
-      res.send('Path is only avaialable to Authenticated Users!');
-  }
-}
-
 // // API Routes
 router.use("/api", apiRoutes);
 router.use("/auth", authRoutes);
--- routes/test/test.js
+++ routes/test/test.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
 const testController = require("../../controllers/testController");
 
-router.get("/", testController.testMsg);
+// Middleware to check if the user is authenticated
+function isUserAuthenticated(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.send('Path is only avaialable to Authenticated Users!');
+    }
+}
+
+router.get("/", isUserAuthenticated, testController.testMsg);
 
 module.exports = router;
