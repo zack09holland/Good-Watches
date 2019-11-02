cae345b9dffcaeb7701edb148674db8eecec489c
devcwilkey <DevCWilkey@gmail.com>
2019-11-02 12:10:14 -0700

    Oh Boi, We can now Auth with Twitter or Google

--- controllers/testController.js
+++ controllers/testController.js
@@ -1,9 +1,5 @@
 module.exports = {
     testMsg: function(req,res){
-        //console.log(req);
-        //console.log('Current Token: ' + req.session.passport.user.token);
-        //console.log('Current User Id: ' + req.session.passport.user.profile.id);
-        //console.log(req.user, 'asldhfwevakljedgfapoiuvawdv');
         res.json({'user' : req.user});
     }
 };
