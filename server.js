a574922db3931f6940d48ac9e5caddd833b1b3b5
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-11-04 21:38:08 -0800

    mongoose first

--- server.js
+++ server.js
@@ -23,43 +23,43 @@ mongoose.set('useUnifiedTopology', true);
 mongoose.connect(MONGODB_URI, err => {
     if (err) throw err;
     console.log('Mongoose connected');
+
+    // Port Value for Express Application
+    const PORT = process.env.PORT || 8080;
+
+    // loads express into app
+    const app = express();
+
+    // configures express app for urlEncoded and Json support
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    // if the NODE_ENV is production (HEROKU Default) then static load the client/build path
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static('client/build'));
+    }
+
+    // gets Cookie-Session and Cookie-Parser Loaded and configured
+    var expiryDate = new Date(Date.now() + 24 * 60 * 60 * 1000); // 1 Day
+    app.use(cookieSession({
+        name: 'session',
+        keys: [process.env.sessionKey1, process.env.sessionKey1],
+        cookie: {
+            secure: true,
+            httpOnly: true,
+            domain: 'good-watches.herokuapp.com',
+            expires: expiryDate
+        }
+    }));
+    app.use(cookieParser(process.env.sessionKey1));
+
+    // inititialze Passport into express app.use and set configuration
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    // initialize routes into express app.use
+    app.use(router);
+
+    // start app listening on Port
+    module.exports = app.listen(PORT, () => console.log('Listening on ' + PORT));
 });
-
-// Port Value for Express Application
-const PORT = process.env.PORT || 8080;
-
-// loads express into app
-const app = express();
-
-// configures express app for urlEncoded and Json support
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if the NODE_ENV is production (HEROKU Default) then static load the client/build path
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
-
-// gets Cookie-Session and Cookie-Parser Loaded and configured
-var expiryDate = new Date(Date.now() + 24 * 60 * 60 * 1000); // 1 Day
-app.use(cookieSession({
-  name: 'session',
-  keys: [process.env.sessionKey1, process.env.sessionKey1],
-  cookie: {
-    secure: true,
-    httpOnly: true,
-    domain: 'good-watches.herokuapp.com',
-    expires: expiryDate
-  }
-}));
-app.use(cookieParser(process.env.sessionKey1));
-
-// inititialze Passport into express app.use and set configuration
-app.use(passport.initialize());
-app.use(passport.session());
-
-// initialize routes into express app.use
-app.use(router);
-
-// start app listening on Port
-module.exports = app.listen(PORT, () => console.log('Listening on ' + PORT));
