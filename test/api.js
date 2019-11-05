5c1d3704d5c734253ba8ece21a1ae3c16b1797b2
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-11-04 20:12:40 -0800

    testing

--- test/api.js
+++ test/api.js
@@ -7,16 +7,16 @@ const request = require('supertest'),
 describe('Title Search', function () {
     it('finds movies by title', function (done) {
         request(server)
-            .put('/api/movies')
-            .send({
-                title: 'The Wind'
-            })
+            .get('/api/movies/search/The Wind')
             .end(function (err, res) {
+                console.error(err);
+                console.log(res.body);
                 assert(Array.isArray(res.body));
                 done();
             });
     });
     after(function () {
         server.close();
+        process.exit();
     })
 });
