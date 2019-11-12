8f89eb499325177a24586546026e7b0703781fd9
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-11-12 08:39:44 -0800

    linting complete

--- routes/test/index.js
+++ routes/test/index.js
@@ -1,23 +1,23 @@
-const router = require("express").Router();
-const testController = require("../../controllers/testController");
+const router = require('express').Router()
+const testController = require('../../controllers/testController')
 
 // Middleware to check if the user is authenticated
-function isUserAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        next();
-    } else {
-        res.send('Path is only avaialable to Authenticated Users!');
-    }
+function isUserAuthenticated (req, res, next) {
+  if (req.isAuthenticated()) {
+    next()
+  } else {
+    res.send('Path is only avaialable to Authenticated Users!')
+  }
 }
 
 // Middleware to log the current req.user
-function logData(req, res, next) {
-    console.log(req.user);
-    console.log(req.session)
-    next();
+function logData (req, res, next) {
+  console.log(req.user)
+  console.log(req.session)
+  next()
 }
 
-router.get("/user", isUserAuthenticated, testController.testMsg);
-router.get("/*", logData, testController.testMsg);
+router.get('/user', isUserAuthenticated, testController.testMsg)
+router.get('/*', logData, testController.testMsg)
 
-module.exports = router;
+module.exports = router
