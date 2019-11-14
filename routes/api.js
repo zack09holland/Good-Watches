bd3e69729a152b5b5a38932c1e845dfaff22e843
Thomas Rodgers <thomasrodgers271@gmail.com>
2019-11-14 10:12:02 -0800

    variable name mismatch fixed

--- routes/api.js
+++ routes/api.js
@@ -237,7 +237,7 @@ router.get('/recommendations/:_id', (req, res) => {
                             // Insert those movies into the db with titles and years.
                             Movie.insertMany(missingMovies.map(tmdMovie => ({
                                 title: tmdMovie.title,
-                                year: parseInt(result.release_date.slice(0, 4))
+                                year: parseInt(tmdMovie.release_date.slice(0, 4))
                             })),
                                 (error, docs) => {
                                     if (error) console.log('Insert error:', error);
